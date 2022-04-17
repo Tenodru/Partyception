425297173df1f6fd6c874dd153e901c564d5e725
StartoStarMP <61712613+StartoStarMP@users.noreply.github.com>
2022-04-17 04:05:26 -0500

    starting funny avatar man

---
+++ Assets/Scripts/AvatarInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AvatarPart
+{
+    public Sprite partSprite;
+    public Color defaultColor;
+    public Color partColor;
+}
+
+[CreateAssetMenu(fileName = "New Avatar Info", menuName = "AvatarInfo")]
+public class AvatarInfo : ScriptableObject
+{
+    public List<AvatarPart> avatarParts;
+}
