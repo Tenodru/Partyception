850e2f4e6d75c35ccd497d42f14270b650cab1ab
StartoStarMP <61712613+StartoStarMP@users.noreply.github.com>
2022-05-10 03:29:29 -0500

    literally one break

--- Assets/Scripts/LobbyMenu.cs
+++ Assets/Scripts/LobbyMenu.cs
@@ -223,6 +223,7 @@ public class LobbyMenu : MonoBehaviour
                                     break;
                                 }
                             }
+                            break;
                         }
                     }
                 }
